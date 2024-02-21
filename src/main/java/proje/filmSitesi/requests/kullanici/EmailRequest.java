55802c20c83caef3ba2a1971694ce2976785f6c5
ZaferBilen <zafer-bilen@outlook.com>
2024-02-21 14:49:27 +0300

    güncelleme

---
+++ src/main/java/proje/filmSitesi/requests/kullanici/EmailRequest.java
@@ -0,0 +1,14 @@
+package proje.filmSitesi.requests.kullanici;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequest {
+
+	private String email;
+	
+}
